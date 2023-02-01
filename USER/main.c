166b3ea3820c16e491ccdf9546f77511ea8a7f59
dengax <dengax1314@qq.com>
2023-02-01 22:11:53 +0800

    add OLED model

--- USER/main.c
+++ USER/main.c
@@ -1,6 +1,7 @@
 #include "stm32f10x.h"
 #include "led.h"
 #include "delay.h"
+#include "OLED.h"
 
 #define LED_ON   0; //led开启
 #define LED_OFF  1; //led关闭
@@ -9,15 +10,20 @@ int main(void)
 {	
     delay_init();
     LED_Init();
+	OLED_Init();
 
     while(1){
-		LED0 = LED_ON;
-		LED1 = LED_ON;
-		delay_ms(500);
+		OLED_Fill_Fast(0xff); //填充白色
+//		delay_ms(500);
+		OLED_Fill_Fast(0x00); //清屏
 		
-		LED0 = LED_OFF;
-		LED1 = LED_OFF;
-		delay_ms(500);
+//		LED0 = LED_ON;
+//		LED1 = LED_ON;
+//		delay_ms(500);
+//		
+//		LED0 = LED_OFF;
+//		LED1 = LED_OFF;
+//		delay_ms(500);
     }
 
 }
